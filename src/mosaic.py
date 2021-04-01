0cf56304a090dc9013882043044e2635255550f4
Adam Prins <AdamPrins@cmail.carleton.ca>
2021-03-31 22:43:21 -0400

    add Hash Saves
    adds setting hashes that store previously calculated contours and images
    splits drawKMeans into getContours, and drawContours
    adds line thickness

--- src/mosaic.py
+++ src/mosaic.py
@@ -9,40 +9,60 @@ class Mosaic():
         self.canvasUpdate = canvasUpdate
         self.settings = {
             "K": 4,
-            "Area": 4,
+            "Area": 2,
             "LineThickness": 2
         }
         self.mosaics = {}
         self.contours = {}
 
+
     def setImage(self, img):
         self.img = img
         self.mosaics = {}
+        self.contours = {}
         self.update()
+
 
     def set(self, setting, value):
         self.settings[setting] = value
         self.update()
 
+
     def get(self, str):
         return self.settings[str]
 
-    def update(self):
-        mosaic = self.drawKMeansContours()
 
-        #self.mosaic = mosaic
+    def update(self):
+        settingHash = self.getSettingHash()
+        contourHash = self.getContourHash()
+
+        if settingHash in self.mosaics:
+            mosaic = self.mosaics[settingHash]
+        elif contourHash in self.contours:
+            contours = self.contours[contourHash]
+            mosaic = self.drawKMeansContours(contours)
+        else:
+            contours = self.getKMeansContours()
+            mosaic = self.drawKMeansContours(contours)
+
+        self.mosaics[settingHash] = mosaic
         self.canvasUpdate(mosaic)
 
 
-    def drawKMeansContours(self):
-        settingHash = hash(frozenset(self.settings.items()))
-        if settingHash in self.mosaics:
-            return self.mosaics[settingHash]
+    def getSettingHash(self):
+        return hash(frozenset(self.settings.items()))
 
+
+    def getContourHash(self):
+        contourSettings = ('K')
+        contourDic = {k:self.settings[k] for k in contourSettings if k in self.settings}
+        contourHash = hash(frozenset(contourDic.items()))
+
+        return contourHash
+
+
+    def getKMeansContours(self):
         mosaic = self.img
-        imgHSV = cv.cvtColor(mosaic, cv.COLOR_BGR2HSV)
-        areaThreshold = int(self.settings["Area"] *0.001 * self.img.shape[0]*self.img.shape[1])
-
         mosaic = cv.blur(mosaic, (5,5))
 
         # reshape the image to a 2D array of pixels and 3 color values (RGB)
@@ -71,9 +91,20 @@ class Mosaic():
             contours, hierarchy = cv.findContours(segmented_image, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
             fullContours = fullContours + contours
 
+
+        contourHash = self.getContourHash()
+        self.contours[contourHash] = fullContours
+        return fullContours
+
+
+    def drawKMeansContours(self, contours):
+        mosaic = self.img
+        areaThreshold = int(self.settings["Area"]/1000 * self.img.shape[0]*self.img.shape[1])
+        imgHSV = cv.cvtColor(mosaic, cv.COLOR_BGR2HSV)
+
         # Fills the contours with their representative colour
         drawing = np.zeros((mosaic.shape[0], mosaic.shape[1], 3), dtype=np.uint8)
-        for cnt in fullContours:
+        for cnt in contours:
             if cv.contourArea(cnt) > areaThreshold:
                 color = (rng.randint(0,256), rng.randint(0,256), rng.randint(0,256))
 
@@ -81,13 +112,11 @@ class Mosaic():
                 cv.drawContours(mask, cnt, -1, 255, -1)
                 mean = cv.mean(self.img, mask=mask)
 
-                cv.drawContours(drawing, [cnt], 0, mean, -2, cv.LINE_8, hierarchy, 0)
+                cv.drawContours(drawing, [cnt], 0, mean, -2, cv.LINE_8)
 
         # Draws the outline of the contours
-        for cnt in fullContours:
+        for cnt in contours:
             if cv.contourArea(cnt) > areaThreshold:
-                cv.drawContours(drawing, [cnt], 0, (0,0,0), self.settings["LineThickness"], cv.LINE_8, hierarchy, 0)
+                cv.drawContours(drawing, [cnt], 0, (0,0,0), self.settings["LineThickness"], cv.LINE_8)
 
-
-        self.mosaics[settingHash] = drawing
         return drawing
